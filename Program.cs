3769970b343cec6a9f4ad9714707096e432d831f
Tanja Levcenko <tanjox78@gmail.com>
2023-12-14 10:19:10 +0300

    add my first app

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿Random rnd = new Random();
+int cpuRandom = rnd.Next(1, 4);
+
+bool loopActive = true;
+
+int numerOfTries = 0;
+
+while (numerOfTries < 3)
+{
+    Console.WriteLine("Try to quess the cpu number:");
+    int userGuess = Convert.ToInt32(Console.ReadLine());
+    numerOfTries++;
+
+    if(userGuess == cpuRandom)
+    {
+        Console.WriteLine("You won!");
+        break;
+    }
+
+}
+Console.WriteLine($"The number of tries: {numerOfTries}");
+Console.WriteLine("Have a nice day!");
